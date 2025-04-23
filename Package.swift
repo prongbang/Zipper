4f99d30022647b8024fd5bd3494fd5da97cdde68
prongbang <prongbang@gmail.com>
2025-04-23 18:01:45 +0700

    config: update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,20 +1,21 @@
 // swift-tools-version:4.0
 import PackageDescription
 
-#if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
-let dependencies: [Package.Dependency] = []
-#else
-let dependencies: [Package.Dependency] = [.package(url: "https://github.com/Meniny/Zipper.git", .exact("1.0.2"))]
-#endif
-
 let package = Package(
     name: "Zipper",
-    products: [
-        .library(name: "Zipper", targets: ["Zipper"])
+    platforms: [
+        .iOS(.v11)
     ],
-	dependencies: dependencies,
+    products: [
+        .library(
+            name: "Zipper",
+            targets: ["Zipper"],
+        ),
+    ],
     targets: [
-        .target(name: "Zipper"),
-		.testTarget(name: "ZipperTests", dependencies: ["Zipper"])
+        .target(
+            name: "Zipper",
+            path: "Zipper" ,
+        ),
     ]
 )
