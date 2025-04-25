893f7470b6ab0279023beccde3181f8dd9fcef31
prongbang <prongbang@gmail.com>
2025-04-25 14:25:26 +0700

    config: swift package manager

--- Package.swift
+++ Package.swift
@@ -1,21 +1,19 @@
 // swift-tools-version:5.7
 import PackageDescription
-
 let package = Package(
     name: "Zipper",
-    platforms: [
-        .iOS(.v11)
-    ],
     products: [
         .library(
             name: "Zipper",
             targets: ["Zipper"]
-        )
+        ),
     ],
+    dependencies: [],
     targets: [
-        .target(
+        .binaryTarget(
             name: "Zipper",
-            path: "Zipper"
+            url: "https://github.com/prongbang/cocoapods-to-xcframework/releases/download/1.2.0/Zipper.xcframework.zip",
+            checksum: "ea364256a0298732a11489262684a9eeba6abaa54242addbe4eb0a6af79c3883"
         )
     ]
 )
